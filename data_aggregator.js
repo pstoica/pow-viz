faba6f90f995508f3342feb3a1de7b2f14bbbc8d
Vikram Somu <vikster93@gmail.com>
2014-04-11 07:24:19 -0400

    fixed untracked files

--- data_aggregator.js
+++ data_aggregator.js
@@ -9,11 +9,30 @@ DataSource.prototype.getStateData = function getStateData(startDate, endDate, lo
 		var collection = db.collection('weed_data');
 
 		function map(){
-			emit(this.quality, this.ppg);
+			var d = new Date(this.date);
+			var key = (d.getMonth()+1) + '/15/' + d.getFullYear();
+			var val = {
+				quality:this.quality,
+				ppg: this.ppg
+			};
+			emit(key, val);
 		}
 
-		function reduce(key,values){
-			return Array.sum(values)/values.length;
+		function reduce(key,vals){
+			var sums = {low:0, medium:0, high:0},
+				counts = {low:0, medium:0, high:0},
+				reducedVal = {low_avg:0, medium_avg:0, high_avg:0};
+
+			for(var i=0; i<vals.length;i++){
+				sums[vals[i].quality]+=vals[i].ppg
+				counts[vals[i].quality]++;
+			}
+
+			reducedVal['low_avg'] = sums['low']/counts['low'];
+			reducedVal['medium_avg'] = sums['medium']/counts['medium'];
+			reducedVal['high_avg'] = sums['high']/counts['high'];
+
+			return reducedVal;
 		}
 
 		collection.mapReduce(
@@ -21,7 +40,7 @@ DataSource.prototype.getStateData = function getStateData(startDate, endDate, lo
 			reduce,
 			{
 				query: {
-					state:loc, 
+					// state:loc, 
 					date:{$gte: startDate, $lt:endDate}
 				},
 				out: {inline:1}
@@ -36,11 +55,30 @@ DataSource.prototype.getNationalData = function getNationalData(startDate, endDa
 		var collection = db.collection('weed_data');
 
 		function map(){
-			emit(this.quality, this.ppg);
+			var d = new Date(this.date);
+			var key = (d.getMonth()+1) + '/15/' + d.getFullYear();
+			var val = {
+				quality:this.quality,
+				ppg: this.ppg
+			};
+			emit(key, val);
 		}
 
-		function reduce(key,values){
-			return Array.sum(values)/values.length;
+		function reduce(key,vals){
+			var sums = {low:0, medium:0, high:0},
+				counts = {low:0, medium:0, high:0},
+				reducedVal = {low_avg:0, medium_avg:0, high_avg:0};
+
+			for(var i=0; i<vals.length;i++){
+				sums[vals[i].quality]+=vals[i].ppg
+				counts[vals[i].quality]++;
+			}
+
+			reducedVal['low_avg'] = sums['low']/counts['low'];
+			reducedVal['medium_avg'] = sums['medium']/counts['medium'];
+			reducedVal['high_avg'] = sums['high']/counts['high'];
+
+			return reducedVal;
 		}
 
 		collection.mapReduce(
@@ -48,7 +86,7 @@ DataSource.prototype.getNationalData = function getNationalData(startDate, endDa
 			reduce,
 			{
 				query: {
-					date:{$gte: startDate, $lt:endDate}
+					date:{$gt: startDate, $lt:endDate}
 				},
 				out: {inline:1}
 			},
